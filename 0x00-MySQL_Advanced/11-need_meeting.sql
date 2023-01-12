455af556d9a8f654d78a47505e0349b274c73b4f
Victoria-20 <victoriaocon111@gmail.com>
2023-01-12 17:26:55 +0300

    fix issue

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,5 @@
 -- create view statement
 CREATE VIEW need_meeting AS
 SELECT * FROM students
-WHERE score < 80 AND last_meeting IS NULL OR last_meeting > 1;
+WHERE score < 80
+AND (last_meeting IS NULL OR DATEDIFF(month, last_meeting, GETDATE()) > 1);
