7f6745d8d1a18efa0237f5e0467dd7234553b55c
Victoria-20 <victoriaocon111@gmail.com>
2023-01-14 13:41:16 +0300

    Fix issue

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,10 +1,13 @@
 -- script that creates a stored procedure AddBonus 
 -- that adds a new correction for a student.
 DELIMITER $$;
-CREATE PROCEDURE AddBonus(IN user_id, project_name, score)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-IF NOT project_name
-project_name = projects.name
-END IF
-END;
+IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name)
+    THEN
+    INSERT INTO projects (name) VALUES (project_name);
+END IF;
 
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES(user_id,(SELECT id from projects WHERE name=project_name), score);
+END;
