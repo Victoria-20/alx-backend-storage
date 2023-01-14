abc0ccae991efacb3b82c281c8cdefe008937857
Victoria-20 <victoriaocon111@gmail.com>
2023-01-14 14:00:25 +0300

    Create view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
 -- create view statement
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE score < 80
 AND (last_meeting IS NULL OR DATEDIFF(month, last_meeting, GETDATE()) > 1);
