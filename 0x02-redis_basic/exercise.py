217c6299869f98ceb9d106d3c91d22f0cbd4c91c
Victoria-20 <victoriaocon111@gmail.com>
2023-01-25 09:07:29 +0300

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    decorator to store the history of inputs and outputs
+    for a particular function
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """stores method's inputs and outputs into redis lists"""
+        self._redis.rpush(f'{key}:inputs', str(args))
+        output = method(self, *args, **kwds)
+        self._redis.rpush(f'{key}:outputs', str(output))
+        return output
+    return wrapper
+
+
 class Cache():
     """ initializes Redis"""
     def __init__(self) -> None:
@@ -26,6 +43,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         r_key = str(uuid.uuid4())
         self._redis.set(r_key, data)
